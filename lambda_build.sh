ea87ef51001fad84ce0d5356494d211e1c73070f
Ephraim Kunz <ephraimkunz@me.com>
2022-02-17 18:28:27 -0800

    Cleanup scripts

--- lambda_build.sh
+++ lambda_build.sh
@@ -1,9 +1,11 @@
-# From https://github.com/hanabu/lambda-web
 
+# Build the container for building a lambda function in.
 docker build -t lambda-builder .
 
+# Cleanup any dangling images.
 docker rmi $(docker images -f "dangling=true" -q)
 
+# Build the lambda function.
 docker run -it --rm \
   -v ~/.cargo/registry:/root/.cargo/registry:z \
   -v "${PWD}":/build:z \
--- lambda_deploy.sh
+++ lambda_deploy.sh
@@ -1,12 +1,3 @@
-# aws lambda create-function --function-name apex \
-#   --handler doesnt.matter \
-#   --zip-file fileb://./lambda.zip \
-#   --runtime provided.al2 \
-#   --role arn:aws:iam::691299561619:role/testLambda \
-#   --environment Variables={RUST_BACKTRACE=1} \
-#   --tracing-config Mode=Active \
-#   --architectures arm64
-
 aws lambda update-function-code \
   --function-name apex-lambda \
   --zip-file fileb://./lambda.zip \
