d627adbf1e00aed2838b67f8caa2a5faaf9f18f0
Ephraim Kunz <ephraimkunz@me.com>
2022-02-17 19:16:46 -0800

    Add ability to print URL after deploy

--- lambda_deploy.sh
+++ lambda_deploy.sh
@@ -1,5 +1,11 @@
+export AWS_PAGER=""
+
 aws lambda update-function-code \
   --function-name apex-lambda \
   --zip-file fileb://./lambda.zip \
   --architectures arm64 \
   --publish
+
+echo "\nAccess the website:"
+aws apigatewayv2 get-apis --no-paginate | jq -r '.Items[0].ApiEndpoint'
+echo "\n"
